f4cace26dd5c3f67363a3f8eb71db802e1561014
iuna Leon <yuna.ads@hotmail.com>
2023-04-06 15:47:02 -0300

    implementandoJS

--- modelo/script.js
+++ modelo/script.js
@@ -3,19 +3,25 @@ function carregar(){
     let img = window.document.getElementById('imagem')
     let data = new Date()
     let hora = data.getHours()
+    
+    
 
     msg.innerHTML = `Agora sao ${hora} horas`
 
     if(hora >= 0 && hora < 12){
         //bom dia
         img.src = './assets/img/manha.png'
+        container.style.background = '#fbf8cc' 
+        
     } else if (hora >= 12 && hora < 18){
         //boa tarde
-        
+
         img.src = './assets/img/tarde.png'
+        container.style.background = '#ffbf69'
     }else{
         //boa noite
         img.src = './assets/img/noite.png'
+        container.style.background = '#01497c'
     }
 
 
